7a4eeae2240e620ef019d695fff864906fe1ce1b
daichiterazawa <teratera@opt.comm.eng.osaka-u.ac.jp>
2021-09-17 18:57:18 +0900

    事前学習

--- signal_process.py
+++ signal_process.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-L = 6
+
 ################　送信ビットを生成　##################
 
 #prbs(２で割った場合の商と剰余を利用する)
@@ -30,26 +30,32 @@ def modQPSK(prbs):
 
 #16QAM
 def modQAM_16(prbs):
+    bits = np.copy(prbs)
+    
     #グレイ符号に伴う（下位ビット）の反転を補正
-    prbs[0::4] ^= prbs[2::4]
-    prbs[1::4] ^= prbs[3::4]
+    bits[0::4] ^= bits[2::4]
+    bits[1::4] ^= bits[3::4]
     #変調
-    prbs = prbs *2 - 1
-    QAM_16 = 1*prbs[0::4] + 1j*prbs[1::4] + 2*prbs[2::4] + 2j*prbs[3::4]
+    bits = bits *2 - 1
+    QAM_16 = 1*bits[0::4] + 1j*bits[1::4] + 2*bits[2::4] + 2j*bits[3::4]
     
     return QAM_16
 
 #64QAM
 def modQAM_64(prbs):
+    bits = np.copy(prbs)
+    
     #グレイ符号化に伴う（中位ビット）の反転補正
-    prbs[2::6] ^= prbs[4::6]
-    prbs[3::6] ^= prbs[5::6]
+    bits[2::6] = bits[2::6]^bits[4::6]
+    bits[3::6] = bits[3::6]^bits[5::6]
+    
     #グレイ符号に伴う（下位ビット）の反転を補正
-    prbs[0::6] ^= prbs[2::6]
-    prbs[1::6] ^= prbs[3::6]
+    bits[0::6] = bits[0::6]^bits[2::6]
+    bits[1::6] = bits[1::6]^bits[3::6]
+    
     #変調
-    prbs = prbs*2 -1
-    QAM_64 = 1*prbs[0::6] + 1j*prbs[1::6] + 2*prbs[2::6] + 2j*prbs[3::6] + 4*prbs[4::6] + 4j*prbs[5::6]
+    bits = bits*2 -1
+    QAM_64 = 1*bits[0::6] + 1j*bits[1::6] + 2*bits[2::6] + 2j*bits[3::6] + 4*bits[4::6] + 4j*bits[5::6]
     
     return QAM_64
 
@@ -57,14 +63,65 @@ def modQAM_64(prbs):
 
 ###################　雑音の付与　#####################
     
-def noise(SNR,N):
+def noise(SNRdB,N,QAM_order):
+    SNR = 1/QAM_order *10**(0.1 * SNRdB)
     noise =  np.random.normal(0, np.sqrt(1/(2*SNR)), N) + 1j*np.random.normal(0, np.sqrt(1/(2*SNR)), N)
     return noise
     
 ###################################################    
 
 ################### 信号の復調 #####################
-
+#QPSA
+def decQPSK(signal_AWGN):
+    #デコードしたビットを格納する配列
+    decQPSK = np.zeros(2 * signal_AWGN.size, dtype = int)
+    
+    #実数成分の閾値処理
+    num_real = signal_AWGN.real
+    num_real[np.where(num_real > 0)] = 1
+    num_real[np.where(num_real < 0)] = 0
+    
+    #虚数成分の閾値処理
+    num_imag = signal_AWGN.imag
+    num_imag[np.where(num_imag > 0)] = 1
+    num_imag[np.where(num_imag < 0)] = 0
+    
+    decQPSK[0::2] = num_real
+    decQPSK[1::2] = num_imag
+    
+    
+    return decQPSK
+#16QAM
+def decQAM_16(signal_AWGN):
+    #デコードしたビットを格納する配列
+    decQAM_16 = np.zeros(4 * signal_AWGN.size, dtype = int)
+    
+    #実数成分の閾値処理
+    num_real = signal_AWGN.real
+    num_real = (num_real // 2) *2 +1
+    num_real[np.where(num_real > 3)] = 3
+    num_real[np.where(num_real < -3)] = -3
+    #虚数成分の閾値処理
+    num_imag = signal_AWGN.imag
+    num_imag = (num_imag // 2) *2 +1
+    num_imag[np.where(num_imag > 3)] = 3
+    num_imag[np.where(num_imag < -3)] = -3
+    
+    #シンボル->ビット
+    #第1象限のみに縮小
+    num_real = (num_real+3)/2
+    num_imag = (num_imag+3)/2
+    #10進数を2進数に
+    decQAM_16[3::4],num_imag = divmod(num_imag,2)
+    decQAM_16[2::4],num_real = divmod(num_real,2)
+    decQAM_16[1::4] = num_imag
+    decQAM_16[0::4] = num_real
+    
+    #グレイ符号のため
+    decQAM_16[0::4] ^= decQAM_16[2::4]
+    decQAM_16[1::4] ^= decQAM_16[3::4]
+    
+    return decQAM_16
 #64QAM
 def decQAM_64(signal_AWGN):
     #デコードしたビットを格納する配列
@@ -103,15 +160,52 @@ def decQAM_64(signal_AWGN):
     return decQAM_64
         
         
+if __name__ == '__main__':  
     
     
-aa = prbs(L)
-a = modQAM_64(aa)
-b = noise(20,a.size) + a
-c= decQAM_64(b)
+    #送信ビット
+    prbs_QPSK = prbs(2 * 20)
+    prbs_QAM16 = prbs(4 * 20)
+    prbs_QAM64 = prbs(6 * 20)
+    
+    #変調    
+    mod_QPSK = modQPSK(prbs_QPSK)
+    mod_QAM16 = modQAM_16(prbs_QAM16)
+    mod_QAM64 = modQAM_64(prbs_QAM64) 
+    
+    #リスト作成
+    SNR_list = np.arange(0,30,1)
+    BER_QPSK_list = []
+    BER_16QAM_list = []
+    BER_64QAM_list = []
+    
+    for SNR in SNR_list:
+        #雑音付与
+        noise_QPSK = mod_QPSK + noise(SNR, mod_QPSK.size, 2*10)
+        noise_QAM16 = mod_QAM16 + noise(SNR, mod_QAM16.size, 4*10)
+        noise_QAM64 = mod_QAM64 + noise(SNR, mod_QAM64.size,6*10)
+        
+        #デコード
+        dec_QPSK = decQPSK(noise_QPSK)
+        dec_QAM16 = decQAM_16(noise_QAM16)
+        dec_QAM64 = decQAM_64(noise_QAM64)
+        
+        BER_QPSK = np.sum(prbs_QPSK^dec_QPSK)/prbs_QPSK.size
+        BER_QAM16 = np.sum(prbs_QAM16^dec_QAM16)/prbs_QAM16.size
+        BER_QAM64 = np.sum(prbs_QAM64^dec_QAM64)/prbs_QAM64.size
+        
+        BER_QPSK_list.append(BER_QPSK)
+        BER_16QAM_list.append(BER_QAM16)
+        BER_64QAM_list.append(BER_QAM64)
+        
+    fig = plt.figure(figsize = (10,10))
+    plt.plot(SNR_list, BER_QPSK_list,marker='.')
+    plt.plot(SNR_list, BER_16QAM_list,marker='.')
+    plt.plot(SNR_list, BER_64QAM_list,marker='.')
+    plt.yscale('log')
+    fig.savefig("img.png")
+        
+        
 
 
-fig = plt.figure(figsize = (10,10))
-plt.scatter(b.real,b.imag)
-plt.show()
 
